0914cecd0389784666fe9bd02225789571da0351
Yashrd2002 <94217776+Yashrd2002@users.noreply.github.com>
2023-10-03 22:50:06 +0530

    first committ

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,16 +12,16 @@ const Pagination = ({id}) => {
           <FaArrowLeftLong />
         </div>}
         
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==1?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/1")}>1</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==2?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/2")}>2</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==3?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/3")}>3</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==4?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/4")}>4</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==5?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/5")}>5</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==6?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/6")}>6</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==7?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/7")}>7</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==8?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/8")}>8</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==9?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/9")}>9</div>
-        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==10?"bg-[#0859ca] text-white":""}`} onClick={()=>navigate("/page/10")}>10</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==1&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/1")}>1</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==2&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/2")}>2</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==3&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/3")}>3</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==4&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/4")}>4</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==5&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/5")}>5</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==6&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/6")}>6</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==7&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/7")}>7</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==8&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/8")}>8</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==9&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/9")}>9</div>
+        <div className={`border-2 text-sm bg-white shadow flex items-center justify-center w-7 h-7 rounded-lg cursor-pointer ${id==10&&"bg-[#1352a9] text-white"}`} onClick={()=>navigate("/page/10")}>10</div>
         {id ==10 ||<div className="border-2 text-sm bg-white shadow flex items-center justify-center cursor-pointer w-7 h-7 rounded-lg" onClick={()=>navigate(`/page/${parseInt(id)+1}`)}>
           <FaArrowRightLong />
         </div>}
